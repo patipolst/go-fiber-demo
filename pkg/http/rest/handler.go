3c0a093e153ecd99935f491b9c74ea7282814d40
patipol <patipol.st@gmail.com>
2020-05-07 22:13:38 +0700

    Add more comment to handler

--- pkg/http/rest/handler.go
+++ pkg/http/rest/handler.go
@@ -5,7 +5,7 @@ import (
 	"github.com/patipolst/go-fiber-demo/pkg/user"
 )
 
-// Handler returns a fiber instance with defined handlers
+// Handler returns a fiber instance with defined api handlers
 func Handler(u user.Service) *fiber.App {
 	app := fiber.New()
 
