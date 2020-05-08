48dc0dad8bc870a039bf8aea69b50ca4de0cc67f
patipol <patipol.st@gmail.com>
2020-05-08 16:37:12 +0700

    Update stub

--- cmd/user-api/main.go
+++ cmd/user-api/main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"github.com/patipolst/go-fiber-demo/pkg/http/rest"
-	"github.com/patipolst/go-fiber-demo/pkg/store/db"
+	"github.com/patipolst/go-fiber-demo/pkg/store/stub"
 	"github.com/patipolst/go-fiber-demo/pkg/user"
 )
 
 func main() {
-	// userStore := stub.NewStore()
-	userStore, _ := db.NewUserStore()
+	userStore := stub.NewUserStore()
+	// userStore, _ := db.NewUserStore()
 	userService := user.NewService(userStore)
 	userHandler := rest.NewUserHandler(userService)
 	app := rest.New(userHandler)
--- pkg/store/stub/user.go
+++ pkg/store/stub/user.go
@@ -2,26 +2,17 @@ package stub
 
 import "github.com/patipolst/go-fiber-demo/pkg/user"
 
-type Store struct {
+type UserStore struct {
 	users map[int]user.User
 }
 
-func NewStore() *Store {
-	return &Store{
-		map[int]user.User{
-			1: {
-				Name: "A",
-				Age:  10,
-			},
-			2: {
-				Name: "B",
-				Age:  20,
-			},
-		},
+func NewUserStore() *UserStore {
+	return &UserStore{
+		make(map[int]user.User),
 	}
 }
 
-func (s *Store) GetAllUsers() []user.User {
+func (s *UserStore) GetAllUsers() []user.User {
 	var users []user.User
 	for _, value := range s.users {
 		users = append(users, value)
@@ -29,16 +20,17 @@ func (s *Store) GetAllUsers() []user.User {
 	return users
 }
 
-func (s *Store) GetUser(id int) user.User {
+func (s *UserStore) GetUser(id int) user.User {
 	user := s.users[id]
 	return user
 }
 
-func (s *Store) CreateUser(user user.User) user.User {
-	s.users[10] = user // fixme
+func (s *UserStore) CreateUser(user user.User) user.User {
+	id := len(s.users) + 1
+	s.users[id] = user
 	return user
 }
 
-func (s *Store) DeleteUser(id int) {
+func (s *UserStore) DeleteUser(id int) {
 	delete(s.users, id)
 }
